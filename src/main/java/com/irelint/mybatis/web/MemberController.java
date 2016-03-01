c00c95b937266276c27fc00f93e3ed3309a66c57
chinazhaoht <zhaohongtao@xiaoneng.cn>
2016-03-01 12:45:59 +0800

    servlet3.0注解测试

---
+++ src/main/java/com/irelint/mybatis/web/MemberController.java
@@ -0,0 +1,13 @@
+package com.irelint.mybatis.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Created by dell on 2016/3/1.
+ */
+
+@WebServlet(name = "member",urlPatterns = "{/members}")
+public class MemberController extends HttpServlet {
+
+}
