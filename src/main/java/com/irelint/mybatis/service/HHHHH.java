74283d6a8626864c15f671133eac5d351272ddd6
chinazhaoht <zhaohongtao@xiaoneng.cn>
2016-03-03 11:39:50 +0800

    servlet3.0注解测试

--- src/main/java/com/irelint/mybatis/service/HHHHH.java
+++ src/main/java/com/irelint/mybatis/service/HHHHH.java
@@ -4,4 +4,8 @@ package com.irelint.mybatis.service;
  * Created by dell on 2016/3/3.
  */
 public class HHHHH {
+    public static void main(String[] args){
+        System.out.println("Test22");
+        System.out.println("sadfasdfadfas");
+    }
 }
