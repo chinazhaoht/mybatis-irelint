dcfbe5f0507caa92e3d19dd967dd54769f11357a
chinazhaoht <zhaohongtao@xiaoneng.cn>
2016-03-03 17:33:08 +0800

    删除一个文件

--- src/main/java/com/irelint/mybatis/service/ZookeeperTest.java
+++ src/main/java/com/irelint/mybatis/service/ZookeeperTest.java
@@ -3,9 +3,27 @@ package com.irelint.mybatis.service;
 /**
  * Created by dell on 2016/2/29.
  */
+@SuppressWarnings("checked")
 public class ZookeeperTest {
 
+    private static class Zookeeper{
+        public Zookeeper(){
+            System.out.println("hello");
+            String s = new String("hello");
+            System.out.println(s);
+            s = s + "nihao";
+            System.out.println(s);
+
+            StringBuffer ss = new StringBuffer("sfadfa");
+            System.out.println(ss);
+            ss.append("sfafafadfa");
+            System.out.println(ss);
+            System.out.println(this.getClass().getClass().getClass().getClass());
+        }
+    }
+
     public static void main(String[] args){
+        new Zookeeper();
         System.out.println("Hello World!");
     }
 }
