fc48d3354261474b6e8c0c97d24c3040b26c3ee9
chinazhaoht <zhaohongtao@xiaoneng.cn>
2016-03-07 20:00:37 +0800

    Cutator使用示例

--- src/main/java/com/irelint/mybatis/service/HHHHH.java
+++ src/main/java/com/irelint/mybatis/service/HHHHH.java
@@ -1,8 +1,17 @@
 package com.irelint.mybatis.service;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.concurrent.BlockingDeque;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -12,219 +21,72 @@ import java.util.concurrent.TimeUnit;
  */
 public class HHHHH {
 
+    static final String connectingString = "120.27.127.2:2181,120.27.127.2:3181";
+
+    static CuratorFramework client = null;
+    public static void main(String[] args) throws Exception {
+
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000,3);
+
+        client = CuratorFrameworkFactory.builder()
+                .connectString(connectingString)
+                .sessionTimeoutMs(5000)
+                .retryPolicy(retryPolicy)
+                .namespace("base")
+                .build();
+        client.start();
+
+        final CountDownLatch down = new CountDownLatch(1);
+        for(int i = 0; i <= 10; i++){
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        down.await();
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss:SSS");
+                    String orderNo = sdf.format(new Date());
+                    System.err.println("生成的订单号是:"+orderNo);
+                }
+            }).start();
+        }
+     down.countDown();
 
 
-    public static void main(String[] args){
-        System.out.println("Test22");
-        System.out.println("sadfasdfadfas");
-        final  int max = 0xfffff;
+    }
 
-        byte min = (byte) max;
-        int hh;
-
-
-        ExecutorService executorService = new ThreadPoolExecutor(10, 100, 5000, TimeUnit.DAYS, new BlockingDeque<Runnable>() {
-            public void addFirst(Runnable runnable) {
-
-            }
-
-            public void addLast(Runnable runnable) {
-
-            }
-
-            public boolean offerFirst(Runnable runnable) {
-                return false;
-            }
-
-            public boolean offerLast(Runnable runnable) {
-                return false;
-            }
-
-            public void putFirst(Runnable runnable) throws InterruptedException {
-
-            }
-
-            public void putLast(Runnable runnable) throws InterruptedException {
-
-            }
-
-            public boolean offerFirst(Runnable runnable, long timeout, TimeUnit unit) throws InterruptedException {
-                return false;
-            }
-
-            public boolean offerLast(Runnable runnable, long timeout, TimeUnit unit) throws InterruptedException {
-                return false;
-            }
-
-            public Runnable takeFirst() throws InterruptedException {
-                return null;
-            }
-
-            public Runnable takeLast() throws InterruptedException {
-                return null;
-            }
-
-            public Runnable pollFirst(long timeout, TimeUnit unit) throws InterruptedException {
-                return null;
-            }
-
-            public Runnable pollLast(long timeout, TimeUnit unit) throws InterruptedException {
-                return null;
-            }
-
-            public boolean removeFirstOccurrence(Object o) {
-                return false;
-            }
-
-            public boolean removeLastOccurrence(Object o) {
-                return false;
-            }
-
-            public boolean add(Runnable runnable) {
-                return false;
-            }
-
-            public boolean offer(Runnable runnable) {
-                return false;
-            }
-
-            public void put(Runnable runnable) throws InterruptedException {
-
-            }
-
-            public boolean offer(Runnable runnable, long timeout, TimeUnit unit) throws InterruptedException {
-                return false;
-            }
-
-            public Runnable remove() {
-                return null;
-            }
-
-            public Runnable poll() {
-                return null;
-            }
-
-            public Runnable take() throws InterruptedException {
-                return null;
-            }
-
-            public Runnable poll(long timeout, TimeUnit unit) throws InterruptedException {
-                return null;
-            }
-
-            public Runnable element() {
-                return null;
-            }
-
-            public Runnable peek() {
-                return null;
-            }
-
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            public int size() {
-                return 0;
-            }
-
-            public Iterator<Runnable> iterator() {
-                return null;
-            }
-
-            public void push(Runnable runnable) {
-
-            }
-
-            public int remainingCapacity() {
-                return 0;
-            }
-
-            public int drainTo(Collection<? super Runnable> c) {
-                return 0;
-            }
-
-            public int drainTo(Collection<? super Runnable> c, int maxElements) {
-                return 0;
-            }
-
-            public Runnable removeFirst() {
-                return null;
-            }
-
-            public Runnable removeLast() {
-                return null;
-            }
-
-            public Runnable pollFirst() {
-                return null;
-            }
-
-            public Runnable pollLast() {
-                return null;
-            }
-
-            public Runnable getFirst() {
-                return null;
-            }
-
-            public Runnable getLast() {
-                return null;
-            }
-
-            public Runnable peekFirst() {
-                return null;
-            }
-
-            public Runnable peekLast() {
-                return null;
-            }
-
-            public Runnable pop() {
-                return null;
-            }
-
-            public Iterator<Runnable> descendingIterator() {
-                return null;
-            }
-
-            public boolean isEmpty() {
-                return false;
-            }
-
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            public boolean addAll(Collection<? extends Runnable> c) {
-                return false;
-            }
-
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            public void clear() {
-
+    public void one() throws Exception {
+        String path = "/animal";
+        client.create().forPath(path,"pig".getBytes());
+        final NodeCache cache = new NodeCache(client,path,false);
+        cache.start(true);
+        cache.getListenable().addListener(new NodeCacheListener() {
+            public void nodeChanged() throws Exception {
+                System.out.println("Node data update ,new data "+new String(cache.getCurrentData().getData()));
             }
         });
 
+        client.setData().forPath(path,"unit".getBytes());
+        Thread.sleep(1000);
+        client.delete().deletingChildrenIfNeeded().forPath(path);
+        Thread.sleep(Integer.MAX_VALUE);
+    }
 
+
+    //master选举
+    public void two() throws InterruptedException {
+        LeaderSelector selector = new LeaderSelector(client, "/master_path", new LeaderSelectorListenerAdapter() {
+            public void takeLeadership(CuratorFramework curatorFramework) throws Exception {
+                System.out.println("成为Master角色");
+                Thread.sleep(3000);
+                System.out.println("完成Master操作，释放Master权利");
+            }
+        });
+
+        selector.autoRequeue();
+        selector.start();
+        Thread.sleep(Integer.MAX_VALUE);
     }
 }
