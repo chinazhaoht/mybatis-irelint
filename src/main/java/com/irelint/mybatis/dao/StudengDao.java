f5498062de28468ad953efa1be85c7cd7d74182a
chinazhaoht <zhaohongtao@xiaoneng.cn>
2016-03-07 19:33:55 +0800

    添加了两台zookeeper服务器

--- src/main/java/com/irelint/mybatis/dao/StudengDao.java
+++ src/main/java/com/irelint/mybatis/dao/StudengDao.java
@@ -1,11 +1,7 @@
 package com.irelint.mybatis.dao;
 
 import com.irelint.mybatis.model.Student;
-import org.springframework.stereotype.Repository;
 
-/**
- * Created by zhaoht on 2016-01-24.
- */
 public interface StudengDao {
     public Student select(Integer id);
 }
--- src/main/java/com/irelint/mybatis/service/HHHHH.java
+++ src/main/java/com/irelint/mybatis/service/HHHHH.java
@@ -1,11 +1,230 @@
 package com.irelint.mybatis.service;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by dell on 2016/3/3.
  */
 public class HHHHH {
+
+
+
     public static void main(String[] args){
         System.out.println("Test22");
         System.out.println("sadfasdfadfas");
+        final  int max = 0xfffff;
+
+        byte min = (byte) max;
+        int hh;
+
+
+        ExecutorService executorService = new ThreadPoolExecutor(10, 100, 5000, TimeUnit.DAYS, new BlockingDeque<Runnable>() {
+            public void addFirst(Runnable runnable) {
+
+            }
+
+            public void addLast(Runnable runnable) {
+
+            }
+
+            public boolean offerFirst(Runnable runnable) {
+                return false;
+            }
+
+            public boolean offerLast(Runnable runnable) {
+                return false;
+            }
+
+            public void putFirst(Runnable runnable) throws InterruptedException {
+
+            }
+
+            public void putLast(Runnable runnable) throws InterruptedException {
+
+            }
+
+            public boolean offerFirst(Runnable runnable, long timeout, TimeUnit unit) throws InterruptedException {
+                return false;
+            }
+
+            public boolean offerLast(Runnable runnable, long timeout, TimeUnit unit) throws InterruptedException {
+                return false;
+            }
+
+            public Runnable takeFirst() throws InterruptedException {
+                return null;
+            }
+
+            public Runnable takeLast() throws InterruptedException {
+                return null;
+            }
+
+            public Runnable pollFirst(long timeout, TimeUnit unit) throws InterruptedException {
+                return null;
+            }
+
+            public Runnable pollLast(long timeout, TimeUnit unit) throws InterruptedException {
+                return null;
+            }
+
+            public boolean removeFirstOccurrence(Object o) {
+                return false;
+            }
+
+            public boolean removeLastOccurrence(Object o) {
+                return false;
+            }
+
+            public boolean add(Runnable runnable) {
+                return false;
+            }
+
+            public boolean offer(Runnable runnable) {
+                return false;
+            }
+
+            public void put(Runnable runnable) throws InterruptedException {
+
+            }
+
+            public boolean offer(Runnable runnable, long timeout, TimeUnit unit) throws InterruptedException {
+                return false;
+            }
+
+            public Runnable remove() {
+                return null;
+            }
+
+            public Runnable poll() {
+                return null;
+            }
+
+            public Runnable take() throws InterruptedException {
+                return null;
+            }
+
+            public Runnable poll(long timeout, TimeUnit unit) throws InterruptedException {
+                return null;
+            }
+
+            public Runnable element() {
+                return null;
+            }
+
+            public Runnable peek() {
+                return null;
+            }
+
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            public int size() {
+                return 0;
+            }
+
+            public Iterator<Runnable> iterator() {
+                return null;
+            }
+
+            public void push(Runnable runnable) {
+
+            }
+
+            public int remainingCapacity() {
+                return 0;
+            }
+
+            public int drainTo(Collection<? super Runnable> c) {
+                return 0;
+            }
+
+            public int drainTo(Collection<? super Runnable> c, int maxElements) {
+                return 0;
+            }
+
+            public Runnable removeFirst() {
+                return null;
+            }
+
+            public Runnable removeLast() {
+                return null;
+            }
+
+            public Runnable pollFirst() {
+                return null;
+            }
+
+            public Runnable pollLast() {
+                return null;
+            }
+
+            public Runnable getFirst() {
+                return null;
+            }
+
+            public Runnable getLast() {
+                return null;
+            }
+
+            public Runnable peekFirst() {
+                return null;
+            }
+
+            public Runnable peekLast() {
+                return null;
+            }
+
+            public Runnable pop() {
+                return null;
+            }
+
+            public Iterator<Runnable> descendingIterator() {
+                return null;
+            }
+
+            public boolean isEmpty() {
+                return false;
+            }
+
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            public boolean addAll(Collection<? extends Runnable> c) {
+                return false;
+            }
+
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            public void clear() {
+
+            }
+        });
+
+
     }
 }
